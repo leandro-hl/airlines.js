5b764fda69e75cd8436fe7c1f1498fe9cd3af6a7
Leandro Herenu <lherenu@coalfire.com>
2021-04-16 00:04:29 -0300

    Inspecting and fixing circular dependencies issues

---
+++ src/moduleManager.ts
@@ -0,0 +1,19 @@
+export * from "./core/identifiable";
+
+//models
+export * from "./bll/model/airline";
+export * from "./bll/model/airlines-registry";
+
+export * from "./bll/model/passenger";
+export * from "./bll/model/plane";
+export * from "./bll/model/flight";
+
+export * from "./bll/model/airport";
+export * from "./bll/model/airports-registry";
+
+export * from "./core/identifierGenerator";
+
+//services
+export * from "./core/repository";
+export * from "./core/bootstrapper";
+export * from "./bll/service";
