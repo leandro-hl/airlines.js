83a4cca60e8a23043b7e050f7aa7cd475c8ece7c
Leandro Herenu <lherenu@coalfire.com>
2021-05-05 16:20:39 -0300

    implementing flights

--- src/bll/flights-rest-service.ts
+++ src/bll/flights-rest-service.ts
@@ -1,16 +1,26 @@
+import { Repository } from "../moduleManager";
+import { Flight } from "./model/flight";
 import { RestService } from "./rest-service";
 
 export class FlightsRestService implements RestService<any> {
+  constructor(private _rep: Repository) {}
+
   getAll() {
-    throw new Error("Method not implemented.");
+    return this._rep.getFlights().getAll();
   }
   get(id: number) {
-    throw new Error("Method not implemented.");
+    return this._rep.getFlights().getDTOBy(id);
   }
   post(item: any) {
-    throw new Error("Method not implemented.");
+    const flight = new Flight(item);
+    const departure = this._rep.getAirports().getBy(item.departure);
+    const arrival = this._rep.getAirports().getBy(item.arrival);
+
+    flight.setDeparture(departure).setArrival(arrival);
+
+    return this._rep.getFlights().add(flight);
   }
   put(item: any) {
-    throw new Error("Method not implemented.");
+    return this._rep.getFlights().update(item);
   }
 }
