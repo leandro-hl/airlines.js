66c459749d7a2b366ff4df3d28ac4610e1e597be
Leandro Herenu <lherenu@coalfire.com>
2021-05-08 16:16:47 -0300

    database setup

--- database/setup.sql
+++ database/setup.sql
@@ -13,26 +13,26 @@ CREATE TABLE IF NOT EXISTS airlines (
 
 CREATE TABLE IF NOT EXISTS passengers (
 	id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-    idCard VARCHAR(50) NOT NULL,
+    id_card VARCHAR(50) NOT NULL,
 	name VARCHAR(100)); 
 
 CREATE TABLE IF NOT EXISTS flights (
 	id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
     departure INT NOT NULL,
-    departureDate TIMESTAMP,
+    departure_date TIMESTAMP,
 	arrival INT NOT NULL,
-    arrivalDate TIMESTAMP,
+    arrival_date TIMESTAMP,
     plane INT,
-    seatsOffered INT,
+    seats_offered INT,
     airline INT NOT NULL); 
 
 CREATE TABLE IF NOT EXISTS flight_scales (
-	idFlight INT NOT NULL, 
-    idAirport INT NOT NULL); 
+	id_flight INT NOT NULL, 
+    id_airport INT NOT NULL); 
 
 CREATE TABLE IF NOT EXISTS flight_passengers (
-	idFlight INT NOT NULL, 
-    idPassenger INT NOT NULL); 
+	id_flight INT NOT NULL, 
+    id_passenger INT NOT NULL); 
 
 
 ALTER TABLE flights ADD CONSTRAINT FK_departure FOREIGN KEY(departure) REFERENCES flights(id);
@@ -40,10 +40,10 @@ ALTER TABLE flights ADD CONSTRAINT FK_arrival FOREIGN KEY(arrival) REFERENCES fl
 ALTER TABLE flights ADD CONSTRAINT FK_plane FOREIGN KEY(plane) REFERENCES planes(id);
 ALTER TABLE flights ADD CONSTRAINT FK_airline FOREIGN KEY(airline) REFERENCES airlines(id);
 
-ALTER TABLE flightScales ADD CONSTRAINT PK_flightScales PRIMARY KEY(idFlight, idAirport);
-ALTER TABLE flightScales ADD CONSTRAINT FK_flights FOREIGN KEY(idFlight) REFERENCES flights(id);
-ALTER TABLE flightScales ADD CONSTRAINT FK_airports FOREIGN KEY(idAirport) REFERENCES airports(id);
+ALTER TABLE flight_scales ADD CONSTRAINT PK_flightScales PRIMARY KEY(id_flight, id_airport);
+ALTER TABLE flight_scales ADD CONSTRAINT FK_flights FOREIGN KEY(id_flight) REFERENCES flights(id);
+ALTER TABLE flight_scales ADD CONSTRAINT FK_airports FOREIGN KEY(id_airport) REFERENCES airports(id);
 
-ALTER TABLE flightPassengers ADD CONSTRAINT PK_flightPassengers PRIMARY KEY(idFlight, idPassenger);
-ALTER TABLE flightPassengers ADD CONSTRAINT FK_flights FOREIGN KEY(idFlight) REFERENCES flights(id);
-ALTER TABLE flightPassengers ADD CONSTRAINT FK_passengers FOREIGN KEY(idPassenger) REFERENCES passengers(id);
+ALTER TABLE flight_passengers ADD CONSTRAINT PK_flightPassengers PRIMARY KEY(id_flight, id_passenger);
+ALTER TABLE flight_passengers ADD CONSTRAINT FK_flights FOREIGN KEY(id_flight) REFERENCES flights(id);
+ALTER TABLE flight_passengers ADD CONSTRAINT FK_passengers FOREIGN KEY(id_passenger) REFERENCES passengers(id);
